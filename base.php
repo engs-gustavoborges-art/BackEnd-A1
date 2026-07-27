50f4d7cebaaf33b828616aa2878f8a319cfa0329
Gustavo <engs-gustavoborges@camporeal.edu.br>
2026-07-27 20:21:21 -0300

    mais coisa

--- base.php
+++ base.php
@@ -6,6 +6,27 @@ echo "Tudo bem? \n";
 // Variaveis
 
 $nome = "Fabricio";
-$idade = 99;
+$idade = 9;
 
-echo "Meu nome é ". $nome . " e tenho ". $idade . " anos de idade.";
+echo "Meu nome é ". $nome . " e tenho ". $idade . " anos de idade. \n";
+
+// Condicionais
+
+if ($idade >= 18 ){
+    echo "já pode joga na blaze!! \n";
+}else{
+    echo "jogue rebox \n";
+}
+
+// Loop
+
+for($i = 0; $i < 10; $i++){
+    echo $i . " ";
+}
+
+// Funções
+
+function calcular($a, $b){
+    return $a + $b;
+}
+echo calcular(10,5);
