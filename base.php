e8fe4ec853d2da9b1c39c93b994a036d1b455221
Gustavo <engs-gustavoborges@camporeal.edu.br>
2026-07-27 20:28:42 -0300

    array top

--- base.php
+++ base.php
@@ -29,4 +29,27 @@ for($i = 0; $i < 10; $i++){
 function calcular($a, $b){
     return $a + $b;
 }
+echo "\n";
 echo calcular(10,5);
+
+// Arrays
+
+$alunos = [
+    [
+        "nome" => "Fabricio",
+        "idade" => 9,
+        "cor" => "verde"
+    ],
+    [
+        "nome" => "Alex",
+        "idade" => 10,
+        "cor" => "azul"
+    ]
+];
+
+foreach($alunos as $aluno){
+    echo "\n ----------------";
+    echo "\n nome: ", $aluno["nome"];
+    echo "\n idade: ", $aluno["idade"];
+    echo "\n cor: ", $aluno["cor"];
+};
